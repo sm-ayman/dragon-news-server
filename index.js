94e3b3d9cb2e8b1207436ef76920be991141e2b1
sm_ayman <sultanmdayman@gmail.com>
2023-07-05 11:23:35 +0600

    news.json updated

--- index.js
+++ index.js
@@ -6,15 +6,37 @@ const cors = require('cors');
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('News API Runnig....');
 })
 
-app.get('/news-categories', (req, res)=>{
+app.get('/news-categories', (req, res) => {
     res.send(categories);
 })
 
-app.listen(port, ()=>{
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id)
+        res.send(categoryNews);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.listen(port, () => {
     console.log('Dragon-News Server listening on port', port);
 })
